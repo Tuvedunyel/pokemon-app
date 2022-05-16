87bdad722d06e21641e14d6daaeb359ca5a79427
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-05-16 16:56:10 +0200

    start card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from "./mock-pokemon";
+import { Pokemon } from "./pokemon";
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  pokemonList = [ 'Bulbizarre', 'Salamèche', 'Carapuce' ]
+  pokemonList: Pokemon[] = POKEMONS
+  pokemonSelected: Pokemon | undefined ;
 
   ngOnInit() {
-    console.table(this.pokemonList)
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(event: HTMLInputElement) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id === Number(event.value));
+    if (pokemon) {
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log("Vous avez demandé un pokémon qui n'existe pas");
+      this.pokemonSelected = pokemon;
+    }
   }
 }
