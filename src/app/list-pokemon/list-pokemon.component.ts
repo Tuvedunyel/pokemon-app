885b052b3822faa1ae71495022ac677b3997d26c
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-05-17 14:41:33 +0200

    routing complete

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from "../pokemon";
 import { POKEMONS } from "../mock-pokemon";
+import { Router } from "@angular/router";
 
 @Component( {
     selector: 'app-list-pokemon',
@@ -10,4 +11,10 @@ import { POKEMONS } from "../mock-pokemon";
 export class ListPokemonComponent {
     pokemonList: Pokemon[] = POKEMONS
     pokemonSelected: Pokemon | undefined;
+
+    constructor (private router: Router) {}
+
+    goToPokemon(pokemon: Pokemon) {
+        this.router.navigate(['/pokemon', pokemon.id]);
+    }
 }
